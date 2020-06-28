4539013c2a04467d935385390c8619b79b6cc6b1
liming <1067746236@qq.com>
2020-06-28 10:14:05 +0800

    提交

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+        <header>
+              傻狗
+        </header>
+    <router-view/>
+  </div>
+</template> 
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+  header{
+    width: 100%;
+    height: 50px;
+    background: red;
+    color: white;
+    line-height: 50px;
+    text-align: center;
+  }
+</style>
